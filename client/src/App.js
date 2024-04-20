b12d9f8ff11ab00a2b179965c233aaae4883bd4e
Hakob3215 <hatajyan@gmail.com>
2024-04-19 22:44:45 -0700

    remove trash

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,7 @@ import SignUp from './pages/SignUp';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import ScanImage from './pages/ScanImage';
-import ScanText from './pages/ScanText';
 import VerifyItems from './pages/VerifyItems';
-import ScanBarcode from './pages/ScanBarcode';
 import GuestNavbar from './components/GuestNavbar';
 
 function App() {
@@ -19,9 +17,7 @@ function App() {
         <Route path="/login" element={<><GuestNavbar/><Login /></>} />
         <Route path="/dashboard" element={<><Dashboard /></>} />
         <Route path="/scan-image" element={<><ScanImage /></>} />
-        <Route path="/scan-text" element={<><ScanText /></>} />
         <Route path="/verify-items" element={<><VerifyItems /></>} />
-        <Route path="/scan-barcode" element={<><ScanBarcode /></>} />
       </Routes>
     </Router>
   );
