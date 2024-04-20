88a7430c1fa6a546759066e3552f48376e011194
Ryan-Kung <ryanling2004@gmail.com>
2024-04-20 13:26:19 -0700

    Login js and css

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './styles/Login.css';
 
 const Login = () => {
     return (
-        <div>
-        <h1>Login</h1>
-        
+        <div className='page'>
+            <form className = 'loginForm'>
+            <h3>Login Here</h3>
+             <label htmlFor="username">Email or Username</label>
+                <input type="text" placeholder="Email or Username" id="username" />
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder="Password" id="password" />
+            <button>Log In</button>
+            </form>
+
         </div>
     );
 };
