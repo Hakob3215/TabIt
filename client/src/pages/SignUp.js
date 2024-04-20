2fd4ffe427ae9787ebee5512e3ae85cc111aae9c
Hakob3215 <hatajyan@gmail.com>
2024-04-19 22:55:03 -0700

    Add a form to SignUp.js, no css

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -3,7 +3,22 @@ import React from 'react';
 const SignUp = () => {
     return (
         <div>
-        <h1>Sign Up Page</h1>
+            <form className="signUpForm">
+                <h1> Sign Up </h1>
+                <label> First Name: </label>
+                <input type="text" name="firstName" required/>
+                <label> Last Name: </label>
+                <input type="text" name="lastName" required/>
+                <label> Email: </label>
+                <input type="email" name="email" required/>
+                <label> Username: </label>
+                <input type="text" name="username" required/>
+                <label> Password: </label>
+                <input type="password" name="password" required/>
+                <label> Confirm Password: </label>
+                <input type="password" name="confirmPassword" required/>
+                <button type="submit"> Sign Up </button>
+            </form>
         </div>
     );
 };
