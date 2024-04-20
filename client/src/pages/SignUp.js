5294ed73628392962f2e2eefecbcef8ecb4fc962
Ryan-Kung <ryanling2004@gmail.com>
2024-04-20 00:30:41 -0700

    sign up styling

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,24 +1,36 @@
 import React from 'react';
-
+import './styles/SignUp.css';
 const SignUp = () => {
     return (
-        <div>
-            <form className="signUpForm">
-                <h1> Sign Up </h1>
-                <label> First Name: </label>
-                <input type="text" name="firstName" required/>
-                <label> Last Name: </label>
-                <input type="text" name="lastName" required/>
-                <label> Email: </label>
-                <input type="email" name="email" required/>
-                <label> Username: </label>
-                <input type="text" name="username" required/>
-                <label> Password: </label>
-                <input type="password" name="password" required/>
-                <label> Confirm Password: </label>
-                <input type="password" name="confirmPassword" required/>
-                <button type="submit"> Sign Up </button>
-            </form>
+    <div>
+    <form className="signUpForm">
+    <h1 className='title'> Sign Up </h1>
+    <fieldset>
+        <legend> First Name: </legend>
+        <input type="text" name="firstName"/>
+    </fieldset>
+    <fieldset>
+        <legend> Last Name: </legend>
+        <input type="text" name="lastName"/>
+    </fieldset>
+    <fieldset>
+        <legend> Email: </legend>
+        <input type="email" name="email"/>
+    </fieldset>
+    <fieldset>
+        <legend> Username: </legend>
+        <input type="text" name="username"/>
+    </fieldset>
+    <fieldset>
+        <legend> Password: </legend>
+        <input type="password" name="password"/>
+    </fieldset>
+    <fieldset>
+        <legend> Confirm Password: </legend>
+        <input type="password" name="confirmPassword"/>
+    </fieldset>
+    <button type="submit"> Sign Up </button>
+</form>
         </div>
     );
 };
