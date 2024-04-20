49621bbd6a87a2bd8f72623cbd408cae487f0ef0
Hakob3215 <hatajyan@gmail.com>
2024-04-19 21:33:58 -0700

    Add Paths for Router

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,13 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<h1>Signing Up</h1>}/>
+        <Route path="/login" element={<h1>Logging In</h1>}/>
+        <Route path="/dashboard" element={<h1>Dashboard</h1>}/>
+        <Route path="/scan-image" element={<h1>Scan Image</h1>}/>
+        <Route path="/scan-text" element={<h1>Scan Text</h1>}/>
+        <Route path="/verify-items" element={<h1>Scan Voice</h1>}/>
+        <Route path="/scan-barcode" element={<h1>Scan Barcode</h1>}/>
       </Routes>
     </Router>
   );
